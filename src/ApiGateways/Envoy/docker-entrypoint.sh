ae8c2f8c406ff0aaeacaf52445db06a64f968db0
Ramaswami, Manikandan <fakeemail@mail.com>
2022-11-28 11:49:43 +0530

    ACR Push and Pull, Included OPA-Envoy

--- src/ApiGateways/Envoy/docker-entrypoint.sh
+++ src/ApiGateways/Envoy/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
+echo "Trying to set /etc permissions"
+sudo chmod 777 /etc
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$ENVOY_CATALOG_API_ADDRESS,\$ENVOY_ORDERING_API_ADDRESS > /etc/envoy.yaml
+sudo cat /tmpl/envoy.yaml.tmpl | envsubst \$ENVOY_CATALOG_API_ADDRESS,\$ENVOY_ORDERING_API_ADDRESS > /etc/envoy.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
